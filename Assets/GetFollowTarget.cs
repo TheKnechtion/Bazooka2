4d190f49bb3bf0c9ee0d03f6875b93b71e6dfa60
aesparza84 <aesparza@colum.edu>
2023-10-15 16:21:10 -0500

    Cinemachine Camera, Canvas scaling but not perfect

--- Assets/GetFollowTarget.cs
+++ Assets/GetFollowTarget.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class GetFollowTarget : MonoBehaviour
 {
+    //This will be the players transform. We can refernce this in case we make the camera 
+    //focus on something else for a little, then head back to the player.
     Transform target;
+
     GameObject playerObj;
     CinemachineVirtualCamera cam;
 
@@ -18,12 +21,14 @@ public class GetFollowTarget : MonoBehaviour
     {
         cam = GetComponent<CinemachineVirtualCamera>();
         playerObj = GameObject.Find("Player");
+
+        target = playerObj.transform;
         setTarget();
     }
 
     private void setTarget()
     {
-        cam.Follow = playerObj.transform;
-        cam.LookAt = playerObj.transform;
+        cam.Follow = target;
+        cam.LookAt = target;
     }
 }
