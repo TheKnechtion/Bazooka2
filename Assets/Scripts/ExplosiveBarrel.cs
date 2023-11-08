5c2345d63692ceda51b12297f3c4c30661203371
aesparza84 <aesparza@colum.edu>
2023-11-07 18:34:08 -0600

    barrel fix, chaining

--- Assets/Scripts/ExplosiveBarrel.cs
+++ Assets/Scripts/ExplosiveBarrel.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class ExplosiveBarrel : DestroyableObject
 {
     [SerializeField] private Explosive explosive;
+    private bool isDestroyed;
 
     // Start is called before the first frame update
     void Start()
     {
+        isDestroyed = false;
         explosive = GetComponent<Explosive>();
     }
 
     public override void Die()
     {
-        explosive.Explode();
-        Destroy(gameObject);
+        if (!isDestroyed) 
+        {
+            isDestroyed= true;
+            explosive.Explode();
+            Destroy(gameObject);
+        }
        
         //Custom explosion delete
     }
