e43dfbfe18bc9c1680d13afc74a293a3322c2493
Xylorific <71472849+Xylorific@users.noreply.github.com>
2023-11-16 15:55:35 -0600

    Added button push

--- Assets/Scripts/Button_Push.cs
+++ Assets/Scripts/Button_Push.cs
@@ -9,7 +9,7 @@ public class Button_Push : MonoBehaviour
     [SerializeField] GameObject activatedObject;
 
 
-    float distanceFromPlayer;
+    public float distanceFromPlayer;
     bool canActivate = false;
 
     Material activatedMat;
@@ -27,12 +27,14 @@ public class Button_Push : MonoBehaviour
     UI_Manager uiManager;
 
 
-    private void Awake()
+    private void Awake()                                    
     {
         PlayerManager.OnPlayerActivatePress += Activate;
 
         activatedMat = (Material)Resources.Load("Activated");
         buttonRenderer = gameObject.GetComponent<MeshRenderer>();
+
+        OnPlayerOutOfRange?.Invoke(this, EventArgs.Empty);
 
         //uiManager = GameObject.Find("Canvas").GetComponent<UI_Manager>();
 
@@ -44,6 +46,7 @@ public class Button_Push : MonoBehaviour
     {
         distanceFromPlayer = (gameObject.transform.position - PlayerInfo.instance.playerPosition).magnitude;
 
+        /*
         if (distanceFromPlayer <= 3.0f)
         {
             canActivate = true;
@@ -52,11 +55,31 @@ public class Button_Push : MonoBehaviour
         else
         {
             canActivate = false;
-            Debug.Log(distanceFromPlayer);
-            OnPlayerOutOfRange?.Invoke(this, EventArgs.Empty);
+            OnPlayerInRange?.Invoke(this, EventArgs.Empty);
         }
+        */
+
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            canActivate = true;
+            OnPlayerInRange?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canActivate = false;
+            OnPlayerOutOfRange?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
 
     public void Activate(object sender, System.EventArgs e)
     {
