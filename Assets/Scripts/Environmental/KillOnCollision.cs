cf11d0e85dadb7d3428c953015b65f7d6f1075e0
marpl6967 <72100598+marpl6967@users.noreply.github.com>
2024-04-29 13:26:13 -0500

    Deco factory

--- Assets/Scripts/Environmental/KillOnCollision.cs
+++ Assets/Scripts/Environmental/KillOnCollision.cs
@@ -6,15 +6,10 @@ public class KillOnCollision : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-
-        }
-        else
-        {
+       
             other.transform.TryGetComponent<IDamagable>(out IDamagable entity);
             entity.TakeDamage(100);
-        }
+        
        
     }
 }
